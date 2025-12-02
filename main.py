c23832dc76c397c5dd02ef7d63ff69d85964a56f
rmansurbek2197 <rmansurbek2197@gmail.com>
2025-12-02 15:20:14 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#1
+# check_number = lambda x: "musbat" if x > 0 else ("manfiy" if x < 0 else "nol")
+#
+# print(check_number(5))
+# print(check_number(-3))
+# print(check_number(0))
+
+#2
+# uchburchak = lambda asos, balandlik: 0.5 * asos * balandlik
+#
+# print(uchburchak(20, 14))
+
+#3
+# is_even = lambda x: x % 2 == 0
+#
+# print(is_even(8887))
+# print(is_even(998))
+
+#4
+# katta_kichik = lambda x: 'katta' if x > 10 else 'kichik'
+#
+# print(katta_kichik(12))
+
+#5
+# check_a = lambda name: 'a' in name.lower()
+# print(check_a('Maksim'))
